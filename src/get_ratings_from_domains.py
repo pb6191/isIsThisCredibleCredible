77331419f94d9ec0249df37ce9649a0ae22d3b55
hauselin <hauselin@gmail.com>
2022-01-31 10:41:51 -0500

    Rename stuff

---
+++ src/get_ratings_from_domains.py
@@ -0,0 +1,31 @@
+# %% load modules
+
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import utils
+
+pd.set_option(
+    "display.max_rows",
+    8,
+    "display.max_columns",
+    None,
+    "display.width",
+    None,
+    "display.expand_frame_repr",
+    True,
+    "display.max_colwidth",
+    None,
+)
+
+np.set_printoptions(
+    edgeitems=5,
+    linewidth=233,
+    precision=4,
+    sign=" ",
+    suppress=True,
+    threshold=50,
+    formatter=None,
+)
+
+#%% read domains
