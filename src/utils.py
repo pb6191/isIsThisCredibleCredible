0ecfab5ef528188fb86af25eac54596ed99a8e13
Puneet Bhargava <pbhargava936@gmail.com>
2022-01-31 12:33:08 -0800

    fetch domain scores

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ def get_score(url):
     endpoint = "https://forum.psci.me/article/credible"
     params = {"url": urllib.parse.quote(url)}
     headers = {"browser_uuid": "is_credible"}
+    data = {"key": "value", "i": 0, "url": "none"}
     resp = requests.get(endpoint, params=params, headers=headers)
     if resp:
         data = resp.json()
